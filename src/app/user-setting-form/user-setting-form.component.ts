29580b3f703e883efebf200613c416e6a395834b
MohamedMF2 <furious_fouad@yahoo.com>
2022-05-31 04:10:08 +0200

    added a data model UserSettings

--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-setting';
 
 @Component({
   selector: 'app-user-setting-form',
   templateUrl: './user-setting-form.component.html',
-  styleUrls: ['./user-setting-form.component.css']
+  styleUrls: ['./user-setting-form.component.css'],
 })
 export class UserSettingFormComponent implements OnInit {
+  userSetting: UserSettings = {
+    name: 'milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    notes: 'Here are some notes',
+    subscriptionType:'Annual'
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
